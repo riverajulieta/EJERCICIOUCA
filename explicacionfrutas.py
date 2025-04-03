e71fc4cc0fff8ce5bf2761baad215d40c7c1bd0f
riverajulieta <julietarivera0@uca.edu.ar>
2025-04-03 19:23:09 -0300

    Add files via upload

---
+++ explicacionfrutas.py
@@ -0,0 +1,9 @@
+
+frutas = ["manzana", "Pera", "Uva", "Mandarina"]
+frutas.apprend("banana")
+frutas.insert(2, "Durazno")
+frutas.remove("Uva")
+frutas.sort()
+frutas.reverse()
+for frutas in frutas
+    print(fruta)
