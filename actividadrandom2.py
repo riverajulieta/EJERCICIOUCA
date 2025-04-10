90f528b8d52b0dc98896baf15c382393f534ab30
riverajulieta <julietarivera0@uca.edu.ar>
2025-04-10 19:28:34 -0300

    Add files via upload

---
+++ actividadrandom2.py
@@ -0,0 +1,13 @@
+import random
+lista = []
+
+for i in range(20):
+    numero = random.randint(1,100)
+    if numero not in lista:
+    lista.append(numero)
+longitud=len(Lista)
+print(lista)
+
+
+
+    
