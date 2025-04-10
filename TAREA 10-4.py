cd4504f548fd479c33ac04f38a8b87724da7b619
riverajulieta <julietarivera0@uca.edu.ar>
2025-04-10 14:12:48 -0300

    Add files via upload

---
+++ TAREA 10-4.py
@@ -0,0 +1,12 @@
+numero1 = int(input ("ingrese su primer número")) 
+numero2 = int(input ("ingrese su segundo número")) 
+print("¿Cuál de los dos es mayor?")
+
+if numero1>numero2:
+    print("El primer número es mayor que el segundo")
+    
+if numero2>numero1:
+    print("El segundo numero es mayor que el primero")
+    
+else:
+    print("Ambos numero son iguales")
