4b28f156bf00170984310a5bc5c518d63fdf572b
riverajulieta <julietarivera0@uca.edu.ar>
2025-04-24 18:58:18 -0300

    Add files via upload

---
+++ ejercicio2_24-4.py
@@ -0,0 +1,48 @@
+#generar grilla
+import random
+ 
+def generar_grilla():
+     grilla= []
+     for i in range(46):
+         grilla.append(i)
+         return grilla
+         
+#generar grilla jugador
+def generar_jugador():
+    return random.sample(range(46),6)
+
+jugador1= generar_jugador
+jugador2= generar_jugador
+
+#sorteo
+def sorteo_quini6(grilla, player1, player2):
+    sorteo = []
+    while grilla and player1 and player2:
+        numero_sorteo = random.choice(grilla)
+        sorteo.append(numero_sorteo)
+        print(f"Numero sorteado: {numero_sorteo}")
+        
+        if numero_sorteo in player1:
+            player1.remove(numero_sorteo)
+        print("Player1 tenía el número")
+        
+        if numero_sorteo in player2:
+            player2.remove(numero_sorteo)
+            print("Player2 tenía el número")
+            
+    grilla.remove(numero_sorteo)
+    return sorteo, plater1, player2
+    
+#mostrar resultados 
+def mostrar_resultados(sorteo, player1,player2):
+    print("\n Números sorteados", sorteo)
+    print("Player1 restantes:", player1)
+    print("Player2 restantes:", player2)
+    
+    if not player1 and not player2:
+        print("Empate")
+    elif not player1: 
+        print("Player1 ganó")
+    elif not player2:
+        print("Player2 ganó")
+        
